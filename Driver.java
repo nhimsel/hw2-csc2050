7bc0dc0b3d9eec7b9b851635ac5f940621654fbc
nhimsel <nhimsel@cuw.edu>
2025-01-21 08:42:08 -0600

    init

---
+++ Driver.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Driver
+{
+	public static void main(String[] args) 
+	{
+	    Random r = new Random();
+	    int secretNumber = r.nextInt(100) + 1;
+		GameMaster gm = new GameMaster(secretNumber);
+		gm.playGame();
+	}
+}
